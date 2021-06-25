bruhmiusmomentium/kernel_xiaomi_sweet

.drone: init drone
Well, yes.

Signed-off-by: CloudedQuartz <ravenklawasd@gmail.com>

kernel_setup.sh
@@ -1,0 +1,32 @@
+#!/bin/bash
+#
+# Copyright (c) 2021 CloudedQuartz
+#
+
+# Script to set up environment to build an android kernel
+# Assumes required packages are already installed
+
+# Config
+CURRENT_DIR="$(pwd)"
+KERNELNAME="Quartz"
+KERNEL_DIR="$CURRENT_DIR"
+AK_REPO="https://github.com/CloudedQuartz/AnyKernel3"
+AK_DIR="$HOME/AnyKernel3"
+TC_DIR="$HOME/proton-clang"
+# End Config
+
+# clone_tc - clones proton clang to TC_DIR
+clone_tc() {
+	git clone --depth=1 https://github.com/kdrag0n/proton-clang.git $TC_DIR
+}
+
+# Clones anykernel
+clone_ak() {
+	git clone $AK_REPO $AK_DIR
+}
+# Actually do stuff
+clone_tc
+clone_ak
+
+# Run build script
+. ${CURRENT_DIR}/kernel_build.sh

prorooter007/Android_Kernel_Xiaomi_Sweet_

Signed-off-by: prorooter007 <shreyashwasnik112@gmail.com>

kernel_setup.sh
@@ -1,0 +1,33 @@
+#!/bin/bash
+#
+# Copyright (c) 2021 CloudedQuartz
+#
+
+# Script to set up environment to build an android kernel
+# Assumes required packages are already installed
+
+# Config
+CURRENT_DIR="$(pwd)"
+KERNELNAME="perf"
+KERNEL_DIR="$CURRENT_DIR"
+AK_REPO="https://github.com/TheStaticDesign/AnyKernel3"
+AK_BRANCH="sweet-perf"
+AK_DIR="$HOME/AnyKernel3"
+TC_DIR="$HOME/proton-clang"
+# End Config
+
+# clone_tc - clones proton clang to TC_DIR
+clone_tc() {
+	git clone --depth=1 https://github.com/kdrag0n/proton-clang.git $TC_DIR
+}
+
+# Clones anykernel
+clone_ak() {
+	git clone $AK_REPO $AK_DIR -b $AK_BRANCH
+}
+# Actually do stuff
+clone_tc
+clone_ak
+
+# Run build script
+. ${CURRENT_DIR}/kernel_build.sh